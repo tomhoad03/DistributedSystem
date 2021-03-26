f5c657784e49edf4ba4d93df16015b51b587e2db
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-03-26 23:38:52 +0000

    Sockets worksheet testing

--- Controller.java
+++ Controller.java
@@ -1,18 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@SuppressWarnings("InfiniteLoopStatement")
 public class Controller {
     public static void main(String[] args) {
         try {
-            int cPort = Integer.parseInt(args[0]); // port to listen on
-            int rFactor = Integer.parseInt(args[1]); // replication factor
+            // reading arguments
+            int controllerPort = Integer.parseInt(args[0]);  // port to listen on
+            int replicationFactor = Integer.parseInt(args[1]); // replication factor
             int timeout = Integer.parseInt(args[2]); // timeout wait time
-            int rPeriod = Integer.parseInt(args[3]); // rebalance wait time
+            int rebalancePeriod = Integer.parseInt(args[3]); // rebalance wait time
 
-            System.out.println("The controller port is: " + cPort);
-            System.out.println("The replication factor is: " + rFactor);
-            System.out.println("The timeout wait time is: " + timeout);
-            System.out.println("The rebalancing period is: " + rPeriod);
+            try {
+                // establish controller listener
+                ServerSocket controllerSocket = new ServerSocket(controllerPort);
+                for (;;) {
+                    try {
+                        // establish connection to client
+                        Socket clientSocket = controllerSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        String line;
 
+                        // receive messages from client
+                        while ((line = in.readLine()) != null) {
+                            System.out.println(line + " received");
+                        }
+                        clientSocket.close();
+                    } catch (Exception ignored) { }
+                }
+            } catch (Exception e) {
+                System.out.println("Error: Invalid Socket!");
+            }
         } catch (Exception e) {
-            System.out.println("Invalid Arguments!");
+            System.out.println("Error: Invalid Arguments!");
         }
     }
 }
+
+/*
+ Notes:
+ 1. Client is given, create controller and dstores
+
+ 2. Store - client stores files in every datastore
+ 3. Load - controller gives client requested file (from one datastore)
+ 4. Remove - file removed from every datastore
+ 5. List - gets a list of all the files
+ 6. Storage Rebalancing - each file is stored in r dstores and files are evenly stored (when adding a new dstore and after interval)
+
+ . Launch in terminal, Ctrl-C to close running program
+ */
--- Dstore.java
+++ Dstore.java
@@ -1,18 +1,34 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Dstore {
     public static void main(String[] args) {
         try {
-            int dPort = Integer.parseInt(args[0]); // port to listen on
-            int cPort = Integer.parseInt(args[1]); // controller port
+            // reading arguments
+            int datastorePort = Integer.parseInt(args[0]); // port to listen on
+            int controllerPort = Integer.parseInt(args[1]); // controller port
             int timeout = Integer.parseInt(args[2]); // timeout wait time
             int fileFolder = Integer.parseInt(args[3]); // location of data store
 
-            System.out.println("The dstore port is: " + dPort);
-            System.out.println("The controller port is: " + cPort);
-            System.out.println("The timeout wait time is: " + timeout);
-            System.out.println("The data store location is: " + fileFolder);
+            try {
+                // establish connection to controller
+                Socket socket = new Socket("Tom-Laptop", controllerPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
 
+                // send messages to controller
+                for(int i = 1; i < 10; i++) {
+                    Thread.sleep(1000);
+
+                    out.println("TCP message " + i);
+                    out.flush();
+                    System.out.println("TCP message " + i + " sent");
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Error: Invalid Send!");
+            }
         } catch (Exception e) {
-            System.out.println("Invalid Arguments!");
+            System.out.println("Error: Invalid Arguments!");
         }
     }
 }
